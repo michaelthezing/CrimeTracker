ce086b89fe827b0f80ae7d5899eb866d4af06c63
Michael Telezing <mtelezing23@gmail.com>
2024-08-19 14:46:16 -0700

    Initial Commit

---
+++ CrimeTracker/CrimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrimeTrackerApp.swift
+//  CrimeTracker
+//
+//  Created by Michael Telezing on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct CrimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
